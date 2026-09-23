a36d3db6bc335c84c978a23dd05a9345ea47bf39
TheoMon-Code <theo.b@monlogistics.com>
2026-09-23 11:33:24 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // MON NPA Quiz — Service Worker v26 (theme auto + tablette)
-const CACHE = 'mon-npa-v35';
+const CACHE = 'mon-npa-v36';
 
 self.addEventListener('install', e => {
   e.waitUntil(self.skipWaiting());
